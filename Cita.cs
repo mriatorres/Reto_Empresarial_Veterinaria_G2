1974d00dbb34ade710d7227d7da0e160ec0d966d
lolarenzo2000 <112091651+lolarenzo2000@users.noreply.github.com>
2025-05-07 20:14:07 -0500

    Add files via upload

---
+++ Cita.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agendar_citas_y_cirugias
+{
+    internal class Cita
+    {
+        public Cliente cliente;
+        public Mascota mascota;
+        public Veterinario veterinario;
+        public string tipo; //cita o cirugia?
+        public int precio;
+
+        public Cita(Cliente cliente, Mascota mascota, Veterinario veterinario, string tipo)
+        {
+            this.cliente = cliente;
+            this.mascota = mascota;
+            this.veterinario = veterinario;
+            this.tipo = tipo;
+        }
+
+        public void CheckarPrecio()
+        {
+            switch (tipo)
+            {
+                case "Cita":
+                    precio = 10000;
+                    break;
+                case "Cirugia":
+                    precio = 50000;
+                    break;
+            }
+        }
+    }
+}
