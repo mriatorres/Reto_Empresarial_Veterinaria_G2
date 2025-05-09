d2b9f259791f52ad7ad80aec174495007dbe5850
mriatorres <mariaf7458@gmail.com>
2025-05-08 21:45:11 -0500

    Se agregaron iconos

--- Forms/Forms Veterinario/DashBoardVeterinario.Designer.cs
+++ Forms/Forms Veterinario/DashBoardVeterinario.Designer.cs
@@ -29,11 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // DashBoardVeterinario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "DashBoardVeterinario";
             this.Text = "DashBoardVeterinario";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
--- Forms/Forms Veterinario/DashBoardVeterinario.cs
+++ Forms/Forms Veterinario/DashBoardVeterinario.cs
@@ -16,5 +16,7 @@ namespace SistemaGestionVeterinaria.Forms
         {
             InitializeComponent();
         }
+
+      
     }
 }
